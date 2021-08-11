d8a36f2dbfd1bdb6bd726f46574623d94898cd1c
Stjepan Mamuša <49687548+smamusa@users.noreply.github.com>
2021-08-11 16:45:40 +0200

    Restructure code

--- modules/Banks.py
+++ modules/Banks.py
@@ -1,8 +1,10 @@
 # Import needed utilities
 import logging
-import modules.Utils as Utils
+import traceback
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
+# Import custom utilities
+import modules.Utils as Utils
 
 # Setup logging
 logging.basicConfig(
@@ -10,28 +12,27 @@ logging.basicConfig(
 
 
 class Bank:
-    session = HTMLSession()
-
-    def __init__(self, url, name="noname"):
+    def __init__(self, url, session, name="noname"):
         self.url = url
         self.name = name
+        self.session = session
 
     def getRates(self):
         pass
 
-
-class OtpBanka(Bank):
-
-    def getRates(self):
+    def getResponse(self):
         # Output info
         logging.info(f"Creating session at URL : {self.url}")
         # Get response from exchange rates url
         try:
-            response = Bank.session.get(self.url)
+            response = self.session.get(self.url)
+            logging.info(f"Session created at URL : {self.url}")
+            return response
         except(Exception):
             logging.critical(f"Exception occurred : {Exception}")
-        logging.info(f"Session created at URL : {self.url}")
 
+    @staticmethod
+    def renderHtml(response):
         # Run JavaScript code on webpage
         logging.info("Rendering HTML")
         try:
@@ -40,20 +41,60 @@ class OtpBanka(Bank):
             logging.critical(f"Exception occurred : {Exception}")
         logging.info("HTML rendered")
 
+    # If a web page has a different structure then a specific parse method must be implemented
+    @staticmethod
+    def parseHtml(response):
         logging.info("Parsing HTML")
         try:
             soup = BeautifulSoup(response.html.html, "html.parser")
             td_tags = soup.find_all("td")
             td_text = [tag.text for tag in td_tags]
+            return td_text
         except(Exception):
             logging.critical(f"Exception occurred while parsing {Exception}")
+            traceback.print_stack()
 
-        print(f"Midmarket rate is {td_text[td_text.index('EUR') + 5]}")
 
-        print(
-            f"OTP Banka buy rate (effective) EUR to HRK : {td_text[td_text.index('EUR') + 3]}")
+class OtpBanka(Bank):
+
+    def getRates(self):
+        response = Bank.getResponse(self)
+        # Run JavaScript code on webpage
+        Bank.renderHtml(response)
+        parsedText = Bank.parseHtml(response)
+        try:
+            print(
+                f"Midmarket rate is {parsedText[parsedText.index('EUR') + 5]}")
+            print(
+                f"OTP Banka cash buy rate EUR to HRK : {parsedText[parsedText.index('EUR') + 3]}")
+            print(
+                f"OTP Banka cash sell rate EUR to HRK : {parsedText[parsedText.index('EUR') + 7]}")
+            print(
+                f"OTP Banka foreign buy rate EUR to HRK : {parsedText[parsedText.index('EUR') + 5]}")
+            print(
+                f"OTP Banka foreign buy rate EUR to HRK : {parsedText[parsedText.index('EUR') + 6]}")
+        except:
+            print("Errors occurred while parsing, check logs")
 
 
 class ZABA(Bank):
-    def __init__(self, url):
-        self.url = url
+    def getRates(self, type):
+        """
+        Get rates for ZABA bank.
+
+        Parameters
+        type (string) - Type of rate, can be foreign or cash
+        """
+        response = Bank.getResponse(self)
+        # Run JavaScript code on webpage
+        Bank.renderHtml(response)
+        parsedText = Bank.parseHtml(response)
+        try:
+            eurIndex = parsedText.index('\nEUR')
+            print(
+                f"ZABA {type} buy rate EUR to HRK : {parsedText[eurIndex + 2]}")
+            print(
+                f"ZABA {type} buy sell EUR to HRK : {parsedText[eurIndex + 4]}")
+        except(Exception):
+            print(
+                "Eror occurred while getting ZABA rates : {0}".format(Exception))
--- scrape.py
+++ scrape.py
@@ -1,28 +1,16 @@
 # Import libraries
-import requests
 from bs4 import BeautifulSoup
 # import HTMLSession from requests_html
 from requests_html import HTMLSession
 import modules.Banks as Banks
 
-otp = Banks.OtpBanka('https://www.otpbanka.hr/hr/tecajna-lista')
+# Make sure to use the same session
+session = HTMLSession()
+
+otp = Banks.OtpBanka('https://www.otpbanka.hr/hr/tecajna-lista', session)
 otp.getRates()
 
-# Set the URL you want to webscrape from
-zaba_url = 'https://www.zaba.hr/home/tecajna'
-session = HTMLSession()
-# Use the object above to connect to needed webpage
-response = session.get(zaba_url)
-
-# Run JavaScript code on webpage
-try:
-    response.html.render()
-
-    soup = BeautifulSoup(response.html.html, "html.parser")
-    td_tags = soup.find_all("td")
-    td_text = [tag.text for tag in td_tags]
-
-    print("ZABA midmarket rate EUR to HRK : {0}".format(
-        td_text[td_text.index('\nEUR') + 3]))
-except(Exception):
-    print("Eror occurred while getting ZABA rates : {0}".format(Exception))
+zaba = Banks.ZABA('https://www.zaba.hr/home/tecajna', session)
+zaba.getRates('foreign')
+zaba = Banks.ZABA('https://www.zaba.hr/home/tecajna#pan2', session)
+zaba.getRates('cash')
