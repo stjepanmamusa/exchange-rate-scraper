64c41b71c5eaef9fac3b443fbf12c3d6bca73c52
Stjepan Mamuša <49687548+smamusa@users.noreply.github.com>
2021-08-11 11:03:47 +0200

    Refactor to English

---
+++ date.py
@@ -0,0 +1,5 @@
+import datetime
+
+date = datetime.datetime.now()
+
+print("Date: {0}.{1}.{2}".format(datum.day,datum.month,datum.year))
--- datum.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-datum = datetime.datetime.now()
-
-print("Datum: {0}.{1}.{2}".format(datum.day,datum.month,datum.year))
