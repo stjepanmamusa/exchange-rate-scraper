2ecdef1d892fbaf769366cffc7dadca1bb8e2458
Stjepan Mamuša <49687548+smamusa@users.noreply.github.com>
2021-09-10 21:16:44 +0200

    Update file structure

---
+++ README.md
@@ -0,0 +1,39 @@
+# Currency Exchange Rate Scraper for Croatian Banks
+
+## Screenshot
+
+![Screenshot](https://github.com/smamusa/banke-tecaj-python/blob/master/Screenshot.png?raw=true)
+
+## Banks
+
+- [x] OTP
+- [x] ZABA
+- [ ] Erste
+- [ ] HPB
+- [ ] PBZ
+
+## To-Do
+
+- [ ] Save rates in a txt or csv file
+- [ ] Correct HTML not being parsed in time
+- [ ] Add other rates
+
+### Installation
+
+```cmd
+    git clone https://github.com/smamusa/exchange-rate-scraper.git
+```
+
+:warning: Before starting the CLI install dependencies
+
+- requests
+
+```cmd
+    pip install requests
+```
+
+- requests_html
+
+```cmd
+    pip install requests_html
+```
---
+++ scrape.py
@@ -0,0 +1,30 @@
+# Import libraries
+from requests_html import HTMLSession
+import logging
+# Custom imports
+import modules.Banks as Banks
+from modules.Banks import Utils
+
+# Setup logging
+formatter = logging.Formatter(
+    '%(asctime)s.%(msecs)03d %(levelname)s:%(name)s:%(funcName)s():%(message)s', '%d.%m.%Y %H:%M:%S')
+file_handler = logging.FileHandler(
+    f'logs/log_{Utils.DateUtil.getDate()}.log')
+file_handler.setFormatter(formatter)
+stream_handler = logging.StreamHandler()
+logger = logging.getLogger(__name__)
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
+
+# Make sure to use the same session
+session = HTMLSession()
+logger.info('CREATE HTML SESSION COMPLETED')
+
+otp = Banks.OtpBanka('https://www.otpbanka.hr/hr/tecajna-lista', session)
+otp.getRates()
+
+zaba = Banks.ZABA('https://www.zaba.hr/home/tecajna', session)
+zaba.getRates('foreign')
+zaba = Banks.ZABA('https://www.zaba.hr/home/tecajna#pan2', session)
+zaba.getRates('cash')
